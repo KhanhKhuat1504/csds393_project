d0fae14999492de8e5d3aeb1720cd3bff437c8cd
KhanhKhuat1504 <86178396+KhanhKhuat1504@users.noreply.github.com>
2025-02-27 09:57:22 -0500

    update schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,11 +2,19 @@ import mongoose, { Document, Model, Schema } from 'mongoose';
 
 interface IUser extends Document {
   clerkId: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  gender: string;
   accountCreated: boolean;
 }
 
 const UserSchema: Schema = new Schema({
-  clerkId: { type: String, required: true },
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  first_name: { type: String, default: "" },
+  last_name: { type: String, default: "" },
+  gender: { type: String, default: "" },
   accountCreated: { type: Boolean, default: false }
 });
 
