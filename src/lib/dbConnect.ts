23935da298395097422d95905d83da34fb5257fe
KhanhKhuat1504 <86178396+KhanhKhuat1504@users.noreply.github.com>
2025-02-27 14:59:23 -0500

    webhook connected to db done

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -2,12 +2,6 @@ import mongoose, { Mongoose, ConnectOptions } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
-}
-
 interface MongooseCache {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
