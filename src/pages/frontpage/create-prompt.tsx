9d84735a6f00f3dee456ecd8de51678426ddcde6
Rickylin888 <sxl1813@case.edu>
2025-04-04 12:21:25 -0400

    Update create-prompt.tsx

--- src/pages/frontpage/create-prompt.tsx
+++ src/pages/frontpage/create-prompt.tsx
@@ -82,9 +82,7 @@ export default function CreatePrompt() {
                 </h1>
                 <div className="ml-auto bg-black text-white px-4 py-2 rounded-lg shadow-md">
                     <SignedIn>
-                        <div className="hidden sm:block">
-                            <OrganizationSwitcher afterCreateOrganizationUrl="/dashboard" />
-                        </div>
+                        
                         <UserButton afterSignOutUrl="/" />
                     </SignedIn>
                 </div>
